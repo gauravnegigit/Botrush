0c0fb99dccdddb35c43cd204743092e26cac90fb
Gaurav Negi <97227844+gauravnegigit@users.noreply.github.com>
2026-04-01 00:41:50 +0530

    Update train.py

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ train_transform = transforms.Compose([
     transforms.Resize(128),
     transforms.RandomCrop(128),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomAffine(0, shear=10, scale=(0.8, 1.2)),
+    transforms.RandomAffine(5, shear=10, scale=(0.8, 1.2)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ])
